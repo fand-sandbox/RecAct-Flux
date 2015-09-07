9875c70b8e04a89c3b3b1258365d5daea6522950
fand <fand@gmork.in>
2015-09-07 21:07:45 +0900

    init

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+    return <h1>Hello World!?</h1>;
+  }
+
+}
+
+React.render(<App />, document.getElementById('app'));
